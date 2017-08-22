54f179db7bd5f3eaaa616110d5a7f43406e9bd9c
Carlton Joseph <carlton.joseph@gmail.com>
2017-08-22 14:58:18 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => res.send('Express Works'));
+
+app.listen(app.get('port'), () =>
+  console.log(`Express running on port ${app.get('port')}`));
